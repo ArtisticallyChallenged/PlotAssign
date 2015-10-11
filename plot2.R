781a95937be7569ac23ce36a32f9dedb978aad35
ArtisticallyChallenged <karen.hunt@ninosalvaggio.com>
2015-10-11 11:56:09 -0400

    Added a variable to simply things.

--- plot2.R
+++ plot2.R
@@ -1,12 +1,11 @@
 plot2 <- function(){
 #Read in only data need instead of entire file.
-     data_file<-read.csv2("c://desktop/coursera/
-               R Working Directory/household_power_consumption.txt",
-                              skip = 66637,nrow=2878)
+     file_path <-"C:\Users\Karen\Desktop\Cousera\
+               R Working Directory\household_power_consumption.txt"
+     data_file<-read.csv2(file_path,skip = 66637,nrow=2878)
 #Skipping the 66637 line also skipped title row
 #Created col_title to get tile row back
-     col_tit<-read.csv2("c://desktop/coursera/
-               R Working Directory/household_power_consumption.txt",nrow=1)
+     col_tit<-read.csv2(file_path,nrow=1)
      colnames(data_file)<-colnames(col_tit)
 #Open png device
     # png("plot1.png")
