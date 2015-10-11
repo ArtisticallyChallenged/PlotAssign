aa1d0e3838c507bb1456801686736ac8f31c68c7
ArtisticallyChallenged <karen.hunt@ninosalvaggio.com>
2015-10-11 11:30:09 -0400

    Added Comments

--- plot1.R
+++ plot1.R
@@ -1,7 +1,11 @@
 plot1 <- function(){
+#Read only the data needed
      data_file<-read.csv2("household_power_consumption.txt",
                               skip = 66637,nrow=2878)
      col_tit<-read.csv2("household_power_consumption.txt",nrow=1)
+#Because skipping 66637 Lines will skip the title line
+#col_tit was created to recapture the tile line of 
+#original data file ("household_power_consumption.txt)
      colnames(data_file)<-colnames(col_tit)
      png("plot1.png")
      hist(as.numeric(data_file$Global_active_power), 
